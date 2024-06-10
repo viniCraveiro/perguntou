ca2c93a72a242307dd6aad6436d2705976425466
Vinicius Craveiro <viniciuscraveiroo@gmail.com>
2024-06-10 19:10:25 -0300

    First commit

---
+++ src/main/java/br/edu/unicesumar/perguntou/PerguntouApplication.java
@@ -0,0 +1,13 @@
+package br.edu.unicesumar.perguntou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerguntouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerguntouApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/unicesumar/perguntou/PerguntouApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.unicesumar.perguntou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerguntouApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
